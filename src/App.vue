9c8b160354a23a020f4a575a9768b26ccbe7b51a
sd <leowest1921@gmail.com>
2023-01-02 19:09:53 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,20 @@
-
-
 <template>
-  <div>
-   <router-view></router-view>
-  </div>
+   <div>
+      <router-view></router-view>
+   </div>
 </template>
 <script>
- 
 </script>
 <style scoped>
 .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+   height: 6em;
+   padding: 1.5em;
+   will-change: filter;
 }
 .logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+   filter: drop-shadow(0 0 2em #646cffaa);
 }
 .logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+   filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,27 +1,41 @@
 <template>
-    <div>
-        <h1>detail {{info.title||""}}</h1>
-    </div>
+   <div>
+      <input :id="tagId" type="hidden" style="display: none" :value="info.title" />
+      <h1>detail {{ info.title || "" }}</h1>
+   </div>
 </template>
 <script>
 import fetch from "@/utils/fetch";
+import { useSSRContext } from "vue";
 export default {
-    data(){
-        return {
-            info: {}
-        }
-    },
-    mounted() {
-        console.info("id", this.$route.params);
-        this.get();
-    },
-    methods: {
-        async get(){
-            let id = this.$route.params.id;
-            let res = await fetch("http://localhost:80/api/articles/"+id).then(res=>res.data);
-            console.info("res", res.data);
-            Object.assign(this.info, res.data);
-        }
-    }
-}
+   setup(props, context) {
+      //context.emit("hello", "你好");
+      //console.info("-===>setup", props, context);
+   },
+   data() {
+      return {
+         tagId: "id-" + Math.random().toString(36).slice(2, 8),
+         info: {},
+      };
+   },
+   created() {
+      if (import.meta.env.SSR) {
+         let ctx = useSSRContext();
+         ctx && Object.assign(this.info, ctx.data);
+         this.detailTag = this.info.title;
+      }
+   },
+
+   mounted() {
+      let dom = document.querySelector(`#${this.tagId}`);
+      if (!dom || !dom.value) this.getx();
+   },
+   methods: {
+      async getx() {
+         let id = this.$route.params.id;
+         let res = await fetch("http://localhost:80/api/articles/" + id).then((res) => res.data);
+         Object.assign(this.info, res.data);
+      },
+   },
+};
 </script>
